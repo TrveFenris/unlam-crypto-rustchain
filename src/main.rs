fc5d6965307ea21520a180c53af1572f8bffdf8e
TrveFenris <tinchondrius@gmail.com>
2019-05-06 22:55:33 -0300

    Implements basic API methods

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,43 @@
 extern crate futures;
 extern crate hyper;
 
+mod api;
+mod types;
+
 use futures::{future, Future};
 
 use hyper::client::HttpConnector;
 use hyper::service::service_fn;
-use hyper::{Body, Client, Method, Request, Response, Server, StatusCode};
-
-type GenericError = Box<dyn std::error::Error + Send + Sync>;
-type ResponseFuture = Box<Future<Item = Response<Body>, Error = GenericError> + Send>;
+use hyper::{Body, Client, Method, Request, Server, StatusCode};
 
 static TEXT: &str = "Hello, World!";
 static NOTFOUND: &[u8] = b"Not Found";
 static ADDRESS: &str = "127.0.0.1:1337";
 
-fn responses(_req: Request<Body>, _client: &Client<HttpConnector>) -> ResponseFuture {
+fn responses(_req: Request<Body>, _client: &Client<HttpConnector>) -> types::ResponseFuture {
     match (_req.method(), _req.uri().path()) {
         (&Method::GET, "/") => {
-            let body = Body::from(TEXT);
-            let mut response = Response::builder();
-            Box::new(future::ok(
-                response
-                    .header("Access-Control-Allow-Origin", "http://localhost:3000")
-                    .header("Content-Type", "application/json")
-                    .status(StatusCode::OK)
-                    .body(body)
-                    .unwrap(),
-            ))
+            // Hello world test method
+            api::create_standard_response(Body::from(TEXT), StatusCode::OK)
         }
-        // POST EXAMPLE
-        /*
-        (&Method::POST, "/resource") => {
-            api_post_response(_req) // implement an api_post_response function that gets a request as a parameter
+        (&Method::POST, "/transactions/new") => {
+            // TODO placeholder body, implement transaction creation
+            let body = Body::from("Successfully added a new transaction to the rustchain.");
+            api::get_transactions_new(body)
         }
-        */
+        (&Method::GET, "/blocks") => {
+            // TODO placeholder body, implement a getter for the complete chain
+            let body = Body::from("Getting all the blocks on the rustchain...");
+            api::get_blocks(body)
+        }
+        (&Method::GET, "/blocks/new") => {
+            // TODO placeholder body, implement block creation
+            let body = Body::from("Successfully created a new block on the rustchain.");
+            api::get_blocks_new(body)
+        }
         _ => {
             // Return 404 not found response.
-            let body = Body::from(NOTFOUND);
-            Box::new(future::ok(
-                Response::builder()
-                    .status(StatusCode::NOT_FOUND)
-                    .body(body)
-                    .unwrap(),
-            ))
+            api::create_standard_response(Body::from(NOTFOUND), StatusCode::NOT_FOUND)
         }
     }
 }
